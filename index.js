5275c20a7de6916baed9393efcc3977293ba06ff
Mary Graham <mary.katherine.graham@outlook.com>
2023-03-30 00:12:45 -0700

    fixing keyboard + drawing gallow

--- index.js
+++ index.js
@@ -10,8 +10,21 @@ const drawGallows = () => {
     context.strokeStyle = 'black';
     context.lineWidth = 3;
    
+    //verticle line
     context.moveTo(35, 10);
     context.lineTo(35, 300);
+
+    //top horizontal line
+    context.moveTo(33.5, 10);
+    context.lineTo(200, 10);
+
+    //connector line
+    context.moveTo(198.5, 10);
+    context.lineTo(198.5, 20);
+
+    //bottom horizontal line
+    context.moveTo(2, 298);
+    context.lineTo(80, 298);
 
     context.stroke();
 };
